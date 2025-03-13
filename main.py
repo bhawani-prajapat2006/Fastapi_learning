6e534b41f2dca83f289f3362a06f2f9fb3001219
Bhawani <bhawanishankarp2006@gmail.com>
2025-03-13 11:30:34 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,43 @@
+# from fastapi import FastAPI
+# from src.books.routes import book_router
+
+# version = 'v1'
+
+# app = FastAPI(
+#     title='Bookly',
+#     description='A RESTful API for a book review web service',
+#     version=version,
+# )
+
+# app.include_router(book_router,prefix=f"/api/{version}/books", tags=['books'])
+
+
+
+from fastapi import FastAPI
+from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import initdb
+
+#the lifespan event
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):    
+#     print("Server is starting...")
+#     yield
+#     print("server is stopping")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):    
+    await initdb()
+    yield
+    print("server is stopping")
+
+
+app = FastAPI(
+    lifespan=lifespan # add the lifespan event to our application
+)
+
+app.include_router(
+    book_router,
+    prefix="/books",
+    tags=['books']
+)
---
+++ src/db/main.py
@@ -0,0 +1,26 @@
+# Inside src/db/main.py
+from sqlmodel import create_engine, text
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from src.config import Config
+from sqlmodel import SQLModel
+from src.books.models import Book
+
+
+# engine = AsyncEngine(create_engine(
+#     url=Config.DATABASE_URL,
+#     echo=True
+# ))
+
+engine = create_async_engine(
+    url=Config.DATABASE_URL,
+    echo= True
+)
+
+
+async def initdb():
+    """create our database models in the database"""
+
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)        
